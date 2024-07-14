33c886b3761617d85928ccf17f3949e0195a27b7
AtqiyaHaydar <ashford1524@gmail.com>
2024-07-14 19:08:39 +0700

    feat: remove ig and twt

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,12 +13,6 @@ const Header = () => {
         Atqiya.
       </h1>
       <div className='space-x-4 flex flex-row'> 
-        <Link href="https://www.instagram.com/atqiyahaydar/" target='_blank'>
-          <InstagramIcon />
-        </Link>
-        <Link href="https://x.com/atqiyahaydar/" target='_blank'>
-          <TwitterIcon />
-        </Link>
         <Link href="https://www.linkedin.com/in/atqiyahaydar/" target='_blank'>
           <LinkedinIcon />
         </Link>
